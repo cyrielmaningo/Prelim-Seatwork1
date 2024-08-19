ce37a6753428306021b786109b6a75436b527e98
Cyriel Maningo <95603-22200943@SERVER2.DOMAIN>
2024-08-19 18:31:16 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const user = {
+    fname: "Cyriel",
+    lname: "Maningo",
+    gender: "Male",
+    age: 23
+}
+value1 = user.fname
+value2 = user.lname
+value3 = user.gender
+value4 = user.age
+
+console.log("I am" + " " + value1 + " " + value2 + " " + "and I am " + value4 + " " + " years old!");
